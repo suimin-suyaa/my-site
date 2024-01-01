0b20ac84e4e4420e35a52e7451f11042fb8e689d
ChiakiIzutsu <chiaki9960@icloud.com>
2024-01-01 22:43:40 +0900

    修正

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -4,15 +4,10 @@
  * Automatically included in `./src/main.ts`
  */
 
-// Plugins
-import vuetify from './vuetify'
-import router from '../router'
+import vuetify from "./vuetify";
+import router from "../router";
+import type { App } from "vue";
 
-// Types
-import type { App } from 'vue'
-
-export function registerPlugins (app: App) {
-  app
-    .use(vuetify)
-    .use(router)
+export function registerPlugins(app: App) {
+  app.use(vuetify).use(router);
 }
